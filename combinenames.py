3e8c89f72e5a827caa2587f1a75c9ff3bde3efa8
Elizabeth <elizabethweeks22@gmail.com>
2021-04-07 02:08:38 -0400

    Added some functionality and beautified.

--- combinenames.py
+++ combinenames.py
@@ -4,15 +4,17 @@ import pandas as pd
 
 base = "../names/"
 index = 0
-d = {'name': [], 'sex': [], 'count':[], 'year':[], 'index':[]}
+d = {'name': [], 'sex': [], 'count':[], 'year':[], 'rank':[], 'index':[]}
 df = pd.DataFrame(data=d)
 df.to_csv('./allnames.csv', mode='w', header=True, index = False)
 
 for filename in os.listdir(base):
     if filename.endswith(".txt"):
         year = int(filename.split("yob")[1].split(".txt")[0])
-        read_file = pd.read_csv(base+filename)
+        read_file = pd.read_csv(base+filename, header=None, names = ['name', 'sex', 'count'])
+        read_file = read_file.sort_values(by=['count'], ascending=False)
         read_file[str(year)] = year
-        read_file[str(index)] = [i for i in range(index+1, index+len(read_file)+1)]
-        read_file.to_csv('./allnames.csv', mode='a', header=True, index=False)
+        read_file['rank'] = [i for i in range(0, len(read_file))]
+        read_file[str(index)] = [i for i in range(index, index+len(read_file))]
+        read_file.to_csv('./allnames.csv', mode='a',header=False, index=False)
         index +=len(read_file) + 1
